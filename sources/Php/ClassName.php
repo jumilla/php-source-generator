14f459b562705bfd049b3efbe8254dfa804c4928
Fumio Furukawa <fumio@jumilla.me>
2015-10-12 03:09:34 +0900

    Add source codes.

---
+++ sources/Php/ClassName.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Jumilla\Generators\Php;
+
+class ClassName
+{
+    /**
+     * Class name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @param string $name
+     */
+    public function __construct($name)
+    {
+        $this->name = trim($name, '\\');
+    }
+
+    /**
+     * @return string
+     */
+    public function name()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->name.'::class';
+    }
+}
